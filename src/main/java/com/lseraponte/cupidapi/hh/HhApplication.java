00c9df5c5ca5da130f36157e3389a6d18707d599
lseraponte <luigi.seraponte@gmail.com>
2025-02-17 21:24:50 +0000

    Initial commit

---
+++ src/main/java/com/lseraponte/cupidapi/hh/HhApplication.java
@@ -0,0 +1,13 @@
+package com.lseraponte.cupidapi.hh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhApplication.class, args);
+	}
+
+}
